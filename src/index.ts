76983ea13eb8428c0298835c91239c354dff6510
sanjeev <ssahani@entiovi>
2023-04-24 17:38:17 +0530

    add user ,company and maker on custom google map

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log("hello world!")
+import { Company } from './Company';
+import {User} from './User';
+import { CustomMaps } from './CustomMaps';
+
+const user=new User();
+const company=new Company()
+const customMap= new CustomMaps("map");
+
+customMap.addUserMarker(user)
+customMap.addCompanyMaker(company)
+
+
+
