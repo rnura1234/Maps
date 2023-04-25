c86f4953dcdfdd5deebc3573e389203945788b79
sanjeev <ssahani@entiovi>
2023-04-25 12:47:35 +0530

    addding the windogInfo when we click on the maker

--- src/Company.ts
+++ src/Company.ts
@@ -1,18 +1,31 @@
 import {faker} from '@faker-js/faker'
-export class Company{
+import { Mappable } from './CustomMaps';
+export class Company implements Mappable{
     companyName:string;
     catchPhrase: string;
     location:{
      lat:number,
      lng:number
     }
+    title:string;
     constructor(){
         this.companyName=faker.company.name();
         this.catchPhrase=faker.company.catchPhrase();
         this.location={
             lat:parseFloat(faker.address.latitude()),
             lng:parseFloat(faker.address.longitude())
-        }
+        },
+        this.title="Company Location"
 
     }
+
+    markerContent():string{
+        return `
+        <h2>${this.companyName}</h2>
+        <h3>${this.catchPhrase}
+        `
+    }
+
+
+
 }
--- src/User.ts
+++ src/User.ts
@@ -1,17 +1,24 @@
 import {faker  } from '@faker-js/faker'
-
-export class User{
+import { Mappable } from './CustomMaps';
+export class User implements Mappable{
     name:string;
     location:{
         lat:number,
         lng:number
-    }
+    };
+    title:string;
     constructor(){
         this.name=faker.name.fullName(),
         this.location={
             lat:parseFloat(faker.address.latitude()),
             lng:parseFloat(faker.address.longitude())
-        }
+        },
+        this.title="User Location"
 
+    }
+    markerContent():string{
+        return `
+        <h2>Username: ${this.name}</h2>  
+        `
     }
 }
